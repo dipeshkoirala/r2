cd5d746a026475f7b2f18f3c80cb455eaa297d6a
dipeshkoirala <dipeshkoirala@gmail.com>
2020-09-19 00:01:39 -0400

    changed

--- front-end/src/component/Cardio.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import'../App.css';
-
-export default function Cardio () {
-    return (
-        <h1 className='cardio'>Cardio</h1>
-    )
-}
--- front-end/src/component/Running.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import'../App.css';
-
-export default function Running () {
-    return (
-        <h1 className='running'>Running</h1>
-    )
-}
